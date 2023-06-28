34d0f4f95078c78c3a63c86e42514e5f3c21c9ea
Pothu sasikumarreddy <pothusasikumarreddy143@gmail.com>
2023-06-28 16:35:21 +0530

    those changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularForms';
+  firstname:string;
+  lastname:string;
+  fname:any;
+  lname:any;
+    defaultgender='male';
+      gender=[
+        {id:'1', value:'male'},
+        {id:'2', value:'female'},
+        {id:'3', value:'other'}
+
+      ]
+      @ViewChild('myform') form:NgForm;
+
+  constructor(){
+  
+  }
+  setDefaultValues(){
+   // this.form.value.personalDetails.firstname='john';
+   // this.form.value.personalDetails.lastname='smith';
+    //this.form.value.personalDetails.email='abc@gmail.com';
+
+        // this.form.setValue({
+        //   personalDetails:({
+        //     firstname:'John',
+        //     lastname:'Smith',
+        //     email:'abc@gmail.com'
+
+        //   })
+        // })
+      this.form.form.patchValue({
+        personalDetails:({
+              firstname:'John',
+              lastname:'Smith',
+              email:'abc@gmail.com'
+  
+            })
+        
+      })
+
+
+  }
+
+
+  Onsubmit(form:NgForm){
+    console.log(form);
+  }
 }
